86e5f9b005be035f27c48c130cb72aa9c3766413
Mark Ryan <markryan1988@gmail.com>
2015-12-03 22:20:19 -0500

    Updates to use length width and height instead of Triple

--- src/main/kotlin/com/markwryan/advent/day2/WrappingPaperCalculator.kt
+++ src/main/kotlin/com/markwryan/advent/day2/WrappingPaperCalculator.kt
@@ -14,29 +14,29 @@ public fun main(args: Array<String>) {
     var wrappingTotal = 0
     var ribbonTotal = 0
     text.forEachLine({
-        val dims = it.split("x").map { it -> it.toInt() }.sort()
-        val dimensions = Triple(first = dims[0], second = dims[1], third = dims[2])
-        wrappingTotal += surfaceArea(dimensions) + (extraPaper(dimensions))
-        ribbonTotal += perimeterOfSmallestSide(dimensions) + cubicVolume(dimensions)
+        //sorted, so 'length' & 'width' are always the smallest sides
+        val (length, width, height) = it.split("x").map { it -> it.toInt() }.sort()
+        wrappingTotal += surfaceArea(length, width, height) + (extraPaper(length, width))
+        ribbonTotal += perimeterOfSmallestSide(length, width) + cubicVolume(length, width, height)
     })
 
     println("Wrapping: " + wrappingTotal)
     println("Ribbon: " + ribbonTotal)
 }
 
-fun cubicVolume(dims: Triple<Int,Int,Int>) :Int {
-    return dims.first * dims.second * dims.third
+fun cubicVolume(length: Int, width: Int, height: Int) :Int {
+    return length * width * height
 }
 
-fun perimeterOfSmallestSide(dims: Triple<Int, Int, Int>): Int {
-    return (2 * dims.first) + (2 * dims.second)
+fun perimeterOfSmallestSide(smallest: Int, secondSmallest: Int): Int {
+    return (2 * smallest) + (2 * secondSmallest)
 }
 
-fun extraPaper(dims: Triple<Int, Int, Int>): Int {
-    return dims.first * dims.second
+fun extraPaper(smallest: Int, secondSmallest: Int): Int {
+    return smallest * secondSmallest
 }
 
-fun surfaceArea(dims: Triple<Int,Int,Int>): Int {
-    return (2 * dims.first * dims.second) + (2 * dims.second * dims.third) + (2 * dims.first * dims.third)
+fun surfaceArea(length: Int, width: Int, height: Int): Int {
+    return (2 * length * width) + (2 * width * height) + (2 * length * height)
 }
 
